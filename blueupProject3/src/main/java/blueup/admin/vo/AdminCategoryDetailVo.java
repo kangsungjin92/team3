478ab22d7110ca1b5b1179394f4ceb603dcabfb8
kangsungjin92 <76843634+kangsungjin92@users.noreply.github.com>
2021-12-07 14:03:04 +0900

    second commit

--- blueupProject3/src/main/java/blueup/admin/vo/AdminCategoryDetailVo.java
+++ blueupProject3/src/main/java/blueup/admin/vo/AdminCategoryDetailVo.java
@@ -11,5 +11,7 @@ public class AdminCategoryDetailVo {
 	int category_detail_no; //카테고리디테일 번호
 	String category_name; //카테고리 이름
 	String detailed_category_name; //세부 카테고리명
+	
+	
 }
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
